641cc32e68fb2c48fca87e6917bca9c3cc72e06d
Kashish <kashishdatta01@gmail.com>
2024-12-03 15:20:36 -0800

    Added hyper parameters

---
+++ gpt.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+batch_size = 32
+block_size = 128
+max_iters = 5000
+eval_interval = 300
+learning_rate = 1e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 128
+n_head = 6
+n_layer = 6
+dropout = 0.3
