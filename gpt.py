06b8ca90bb030c6df513b571d3fe7dec18a3b600
Suzanna Thomas <91756576+Suz186@users.noreply.github.com>
2024-12-03 15:54:45 -0800

    Milestone 4 - Multihead Attention

--- gpt.py
+++ gpt.py
@@ -74,3 +74,15 @@ class Head(nn.Module):
         v = self.value(x)
         out = wei @ v
         return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
