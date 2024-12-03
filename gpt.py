b7242c3542574faf0f3d99bfab82a8f87adf1be0
Suzanna Thomas <91756576+Suz186@users.noreply.github.com>
2024-12-03 15:44:05 -0800

    Update-gpt.py
    Hyperparameter Values updated

--- gpt.py
+++ gpt.py
@@ -4,13 +4,13 @@ from torch.nn import functional as F
 
 batch_size = 32
 block_size = 128
-max_iters = 5000
+max_iters = 3000
 eval_interval = 300
-learning_rate = 1e-4
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 128
 n_head = 6
 n_layer = 6
-dropout = 0.3
+dropout = 0.2
 
