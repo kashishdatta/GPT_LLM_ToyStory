f246d562da01aef3c66a6885d5a2345df3871fc3
sarvenazdirakvand <sarvenaz.dirakvand01@gmail.com>
2024-12-03 15:53:45 -0800

    Milestone 3

--- gpt.py
+++ gpt.py
@@ -53,3 +53,24 @@ def estimate_loss():
         out[split] = losses.mean().item()
     model.train()
     return out
+
+class Head(nn.Module):
+    def init(self, head_size):
+        super().init()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        v = self.value(x)
+        out = wei @ v
+        return out
