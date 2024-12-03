e1839c324361b2799c0777a235bfc7b10ab92706
ngbvu <vubichngoc97@gmail.com>
2024-12-03 15:51:26 -0800

    Data loading, train and test

--- gpt.py
+++ gpt.py
@@ -14,3 +14,42 @@ n_head = 6
 n_layer = 6
 dropout = 0.2
 
+torch.manual_seed(1337)
+
+with open('TOY STORY-new-2.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+text = text.replace('\n', ' ').replace('\r', '').strip()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean().item()
+    model.train()
+    return out
